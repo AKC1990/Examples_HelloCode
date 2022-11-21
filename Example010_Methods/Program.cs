e9c0908ebe887cb379ee30bf477e575edbd03af1
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 14:26:55 +0300

    Added tasks from Lesson01 and Lesson02

---
+++ Example010_Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 12, 5, 65, 29, 40, 1, 76, 3, 40 };
+
+int n = array.Length;
+int find = 40;
+
+int index = 0;
+
+while (index < n)
+{
+    if (find == array[index])
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+    
+}
+
